cfd2652ebdb1ee5aaeb69dedcbb04bb44075c31b
mwylegly <michael.wylegly@gmail.com>
2012-05-17 13:45:12 -0600

    Slow running Commit

---
+++ imageRenderProject/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  imageRenderProject
+//
+//  Created by MADT.Student on 12-05-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "C4AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+    												return UIApplicationMain(argc, argv, nil, NSStringFromClass([C4AppDelegate class]));
+    												}
+    											
+}
